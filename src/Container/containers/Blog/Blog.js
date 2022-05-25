0e9d64895622e5a9c843c736770ec13ea1bb671d
robinhood1712 <ademolaadewoleo1@gmail.com>
2022-05-25 21:08:46 +0800

    adding axios to our project and using it to fetch some data from a server

--- src/Container/containers/Blog/Blog.js
+++ src/Container/containers/Blog/Blog.js
@@ -3,20 +3,52 @@ import React, { Component } from 'react';
 import Post from '../../../Components/components/Post/Post';
 import FullPost from '../../../Components/components/FullPost/FullPost';
 import NewPost from '../../../Components/components/NewPost/NewPost';
+// import Axios from ''
 import './Blog.css';
 
 class Blog extends Component {
+
+    constructor(props){
+        super(props)
+          this.state = {
+              posts: []
+            }
+      }
+
+    componentDidMount(){
+        axios.get('https:jsonplaceholder.typeicode.com').then(response => {
+            // console.log(response)
+            const posts = response.data.slice(0, 4)
+            const updatedPost = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'demola'
+                }
+            })
+            this.setState({
+                posts: updatedPost
+            })
+        })
+    }
+
+
 
     
 
 
     render () {
+
+        const posts = this.state.posts.map(post => {
+            return <Post
+                     title = {post.title}
+                     key = {post.id}/>
+        })
+
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
