b683d233eb26db3979815670d9e259344dd60008
robinhood1712 <ademolaadewoleo1@gmail.com>
2022-05-25 10:22:25 +0800

    preparing the project/changing folder structure for axios react practice

--- src/App.js
+++ src/App.js
@@ -1,46 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import Blog from './Container/containers/Blog/Blog';
+// import './App.css';
 
-import UserOutput from './UserOutput';
-import UserInput from './UserInput';
 
 class App extends Component { 
 
   constructor(props){
     super(props)
-      this.state = {
-      username: 'ade'
-    }
+    //   this.state = {
+    //   username: 'ade'
+    // }
   }
   
   
-
-    eventChangeHandlerMethod = (event) => {
-      this.setState({userName: event.target.value});
-    }
-
-    eventClickedHandler = () => {
-      alert('hello');
-      this.setState({userName: 'kunle'})
-    }
-
-  render() {
-    return (
-      <div className='App'>
-        <UserInput  
-          changed = {this.eventChangeHandlerMethod}
-          name = {this.state.username}/>
-        <UserOutput userName = {this.state.username} clicked = {this.eventClickedHandler}/> 
-        <UserOutput userName = {this.state.username} />
-        <button onClick={this.eventClickedHandler}> click me</button> 
-        <UserOutput userName = 'ademola' clicked = {this.eventClickedHandler} />  
-      </div>
-    )
-
+    render() {
+      return (
+        <div className="App">
+          <Blog />
+        </div>
+      );
+    }  
   }
-
-}
-
 
 export default App;
 
---
+++ src/Container/containers/Blog/Blog.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import Post from '../../../Components/components/Post/Post';
+import FullPost from '../../../Components/components/FullPost/FullPost';
+import NewPost from '../../../Components/components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div>
+                <section className="Posts">
+                    <Post />
+                    <Post />
+                    <Post />
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
