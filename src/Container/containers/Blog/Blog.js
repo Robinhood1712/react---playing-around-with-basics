eb94d80dca10fa9c808dae368a82ec025635d88d
robinhood1712 <ademolaadewoleo1@gmail.com>
2022-07-28 23:07:10 +0800

    using navlink to style active classes

--- src/Container/containers/Blog/Blog.js
+++ src/Container/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 // import  axios from 'axios';
 
 class Blog extends Component {
@@ -14,12 +14,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/" >Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink to="/" exact >Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname:this.props.match.url + "/",
                                 hash:"#submit",
                                 search:"?quick-submit=true"
-                            }}>New post</Link></li>
+                            }}>New post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
